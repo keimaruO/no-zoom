dda8505bddb6b22dfdeb8226113d309386003b24
keimaruO <91080250+keimaruO@users.noreply.github.com>
2024-04-06 21:16:27 +0900

    Update content.js

--- content.js
+++ content.js
@@ -1,11 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var metaViewport = document.querySelector('meta[name="viewport"]');
-    if (metaViewport) {
-      metaViewport.setAttribute('content', 'user-scalable=no');
-    } else {
-      metaViewport = document.createElement('meta');
-      metaViewport.setAttribute('name', 'viewport');
-      metaViewport.setAttribute('content', 'user-scalable=no');
-      document.head.appendChild(metaViewport);
+function disableZoom() {
+  let viewportMeta = document.querySelector('meta[name="viewport"]');
+  if (viewportMeta) {
+    if (!/user-scalable=no/.test(viewportMeta.content)) {
+      viewportMeta.content += ", user-scalable=no";
     }
-  });
+  } else {
+    viewportMeta = document.createElement('meta');
+    viewportMeta.name = "viewport";
+    viewportMeta.content = "width=device-width, initial-scale=1, user-scalable=no";
+    document.getElementsByTagName('head')[0].appendChild(viewportMeta);
+  }
+}
+
+// Run the function to disable zooming.
+disableZoom();
